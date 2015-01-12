cdd40675f3943e564eef21c1f93aadb2b19f2270
Brendan Maione-Downing <b.maionedowning@gmail.com>
2015-01-11 23:05:38 -0500

    Function to calculate overlap of two KDEs

---
+++ calculate_kde_overlaps.R
@@ -0,0 +1,28 @@
+library(sfsmisc)
+setwd('~/Dropbox/GIT Projects/text-stats/outputs/20150111-193235_outputs_10000_iterations_chisquare/')
+
+kde_overlap <- function(a, b) {
+	# ensure that kernels are calculated along a common scale
+	lower <- min(c(a, b)) - 1
+	upper <- max(c(a, b)) + 1
+	
+	# generate kernel densities
+	da <- density(a, from=lower, to=upper)
+	db <- density(b, from=lower, to=upper)
+	d <- data.frame(x=da$x, a=da$y, b=db$y)
+	
+	# calculate intersection densities
+	d$w <- pmin(d$a, d$b)
+	
+	# integrate
+	total <- integrate.xy(d$x, d$a) + integrate.xy(d$x, d$b)
+	intersection <- integrate.xy(d$x, d$w)
+	
+	# return % of each curve overlapping
+	return(intersection/total)
+}
+
+df <- read.csv('text_vs_others_comparisons.csv', header=TRUE)
+df2 <- read.csv('self_vs_self_comparisons.csv', header=TRUE)
+
+kde_overlap(df$Chi2.Statistic, df2$Chi2.Statistic)
