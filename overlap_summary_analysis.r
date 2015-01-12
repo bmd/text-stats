d748b629759f972e0ce7b3813dbea70cdc0f8291
Brendan Maione-Downing <bmdowning@bluestatedigital.com>
2015-01-12 16:58:14 -0500

    Finish curve overlap script for #12

--- overlap_summary_analysis.r
+++ overlap_summary_analysis.r
@@ -1,33 +1,37 @@
 library(sfsmisc)
+library(stringr)
 
 # set environments
-BASE_DIR = 'path/to/outputs/'
+BASE_DIR = '~/git/text-stats/outputs'
 setwd(BASE_DIR)
 
-# include fxn to calculate overlap of KDE curves
-source('r_src/calculate_kde_overlaps.r')
+# function to calculate KDE overlaps
+source('../r_src/calculate_kde_overlaps.r')
 
 # pattern match to extract iterations and stopword data, etc
 determine_simulation_parameters_from_file_name <- function (name) {
-	regex = '.*'
-	r <- list(d.match(regex))
-	return (r)
+	library(stringr)
+	
+	# define regex
+	expr <- '[0-9]{8}-[0-9]{6}_IT([0-9]+)_SW([0-9]+)_PCT([0-9]+)_(.*)'
+	# extract matches 
+	matches <- str_match_all(name, expr)
+	
+	return (unlist(matches))
 }
 
 # get all the directories contained in outputs
 results_directories <- dir(path='.')
 
 # create a variable to store summaries
-output <- data.frame(
-	row.names=c('Folder Name', 'Iterations', 'Stopwords', 'Sample Size', 'Curve overlap %'),
-	header=TRUE
-)
+output <- NULL
+output <- rbind(output, c('Folder Name', 'Iterations', 'Stopwords', 'Sample Size', 'Method', 'Curve overlap pct'))
 
 # iterate over all sets of results in the results directory
 for (d in results_directories) {
 	# load data
-	t <- read.csv('text_vs_others_comparisons.csv')
-	s <- read.csv('self_vs_self_comparisons.csv')
+	t <- read.csv(file.path(d, 'text_vs_others_comparisons.csv'))
+	s <- read.csv(file.path(d, 'self_vs_self_comparisons.csv'))
 	
 	# calculate per-curve overlap %:
 	# because the area under both curves is 1, this overlap = % overlap.
@@ -35,11 +39,13 @@ for (d in results_directories) {
 	overlap_pct = kde_overlap(t$Chi2.Statistic, s$Chi2.Statistic)
 	
 	# parse simulation parameters for categorical values
-	names <- determine_simulation_parameters_from_file_name(d)
-	final <- cbind(names, overlap_pct)
+	parameters <- determine_simulation_parameters_from_file_name(d)	
+	parameters[[length(parameters)+1]] <- overlap_pct
+
+	# return result
+	output <- rbind(output, parameters)
 	
-	# append row to final output 
-	output <- rbind(output, final)
 }
 
-write.csv(output)
+# write result to CSV file
+write.csv(output, file='curve_overlap_results.csv', row.names=FALSE)
