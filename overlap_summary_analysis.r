204a1bb9219e9051db7807c25654b22427d25a4b
Brendan Maione-Downing <bmdowning@bluestatedigital.com>
2015-01-13 09:01:29 -0500

    R code now meets Google style guide

--- overlap_summary_analysis.r
+++ overlap_summary_analysis.r
@@ -7,7 +7,7 @@ source('../r_src/calculate_kde_overlaps.r')
 source('../r_src/utils.r')
 
 # get all the directories contained in outputs
-results_directories <- dir(path='.')
+results_directories <- dir(path = '.')
 
 # create a variable to store summaries
 output <- NULL
@@ -15,23 +15,20 @@ output <- rbind(output, c('Folder Name', 'Iterations', 'Stopwords', 'Sample Size
 
 # iterate over all sets of results in the results directory
 for (d in results_directories) {
-	# load data
-	t <- read.csv(file.path(d, 'text_vs_others_comparisons.csv'))
-	s <- read.csv(file.path(d, 'self_vs_self_comparisons.csv'))
+  # load data
+  t <- read.csv(file.path(d, 'text_vs_others_comparisons.csv'))
+  s <- read.csv(file.path(d, 'self_vs_self_comparisons.csv'))
 	
-	# calculate per-curve overlap %:
-	# because the area under both curves is 1, this overlap = % overlap.
-	# 2* this result gets the total overlap area (area under both curves)  
-	overlap_pct = CalculateKDEOverlap(t$Chi2.Statistic, s$Chi2.Statistic)
-	
-	# parse simulation parameters for categorical values
-	parameters <- ListRegexMatches(d, '[0-9]{8}-[0-9]{6}_IT([0-9]+)_SW([0-9]+)_PCT([0-9]+)_(.*)')	
-	parameters[[length(parameters)+1]] <- overlap_pct
+  # calculate the % overlapping area between the two curves
+  overlap_pct <- CalculateKDEOverlap(t$Chi2.Statistic, s$Chi2.Statistic)
 
-	# return result
-	output <- rbind(output, parameters)
-	
+  # parse simulation parameters for categorical values
+  parameters <- ListRegexMatches(d, '[0-9]{8}-[0-9]{6}_IT([0-9]+)_SW([0-9]+)_PCT([0-9]+)_(.*)')	
+  parameters[[length(parameters)+1]] <- overlap_pct
+
+  # return result
+  output <- rbind(output, parameters)	
 }
 
 # write result to CSV file
-write.csv(output, file='curve_overlap_results.csv', row.names=FALSE)
+write.csv(output, file = 'curve_overlap_results.csv', row.names = FALSE)
--- r_src/calculate_kde_overlaps.r
+++ r_src/calculate_kde_overlaps.r
@@ -1,33 +1,32 @@
 CalculateKDEOverlap <- function(a, b) {
-	# Estimates the overlapping area between the kernel density
-	# functions calculated on two vectors.
-	#
-	# Args:
-	#   a: A vector without missing values from which a kernel
-	#      density estimate can be produced.
-	#   b: A vector without missing values from which a kernel
-	#      density estimate can be produced.
-	#
-	# Returns:
-	#   The shared area between the kernel density functions
-	#   of a and b. 
-	library(sfsmisc)
+  # Estimates the overlapping area between the kernel density
+  # functions calculated on two vectors.
+  #
+  # Args:
+  #   a: A vector without missing values from which a kernel
+  #      density estimate can be produced.
+  #   b: A vector without missing values from which a kernel
+  #      density estimate can be produced.
+  #
+  # Returns:
+  #   The shared area between the kernel density functions
+  #   of a and b. 
+  library(sfsmisc)
 	
-	# ensure that kernels are calculated along a common scale
-	upper <- max(c(a, b)) + 1
+  # ensure that kernels are calculated along a common scale
+  upper <- max(c(a, b)) + 1
 	
-	# generate kernel densities
-	da  <- density(a, from = 0, to = upper)
-	db  <- density(b, from = 0, to = upper)
-	d   <- data.frame(x = da$x, a = da$y, b = db$y)
-	d$w <- pmin(d$a, d$b)  # model overlapping region 
+  # generate kernel densities
+  da  <- density(a, from = 0, to = upper)
+  db  <- density(b, from = 0, to = upper)
+  d   <- data.frame(x = da$x, a = da$y, b = db$y)
+  d$w <- pmin(d$a, d$b)  # model overlapping region 
 	
-	# integrate
-	area.under.da <- integrate.xy(d$x, d$a)
-	area.under.db <- integrate.xy(d$x, d$b)
-	intersection  <- integrate.xy(d$x, d$w)
+  # integrate
+  area.under.da <- integrate.xy(d$x, d$a)
+  area.under.db <- integrate.xy(d$x, d$b)
+  intersection  <- integrate.xy(d$x, d$w)
 
-	# return overlapping curve area
-	return((2 * intersection) / (area.under.da + area.under.db))
+  # return overlapping curve area
+  return((2 * intersection) / (area.under.da + area.under.db))
 }
-
